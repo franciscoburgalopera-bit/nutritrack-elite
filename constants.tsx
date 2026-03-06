c1fb257f9c44154d05a5d8cd085bced0ae698e1c
franciscoburgalopera-bit <franciscoburgalopera@gmail.com>
2026-03-06 17:07:00 +0100

    Add constants.tsx

---
+++ constants.tsx
@@ -0,0 +1,9 @@
+
+export const MOCK_RECOMMENDED_FOODS = [
+  { id: '1', name: 'Banana', calories: 105, serving: '1 medium', protein: 1, carbs: 27, fats: 0.3, image: 'https://picsum.photos/seed/banana/200' },
+  { id: '2', name: 'Red Apple', calories: 95, serving: '1 medium', protein: 0.5, carbs: 25, fats: 0.3, image: 'https://picsum.photos/seed/apple/200' },
+  { id: '3', name: 'Oatmeal', calories: 150, serving: '1 cup cooked', protein: 6, carbs: 27, fats: 3, image: 'https://picsum.photos/seed/oatmeal/200' },
+  { id: '4', name: 'Chicken Breast', calories: 165, serving: '100g grilled', protein: 31, carbs: 0, fats: 3.6, image: 'https://picsum.photos/seed/chicken/200' },
+  { id: '5', name: 'Whole Grain Bread', calories: 80, serving: '1 slice', protein: 4, carbs: 12, fats: 1, image: 'https://picsum.photos/seed/bread/200' },
+  { id: '6', name: 'Grilled Salmon', calories: 382, serving: '1 fillet (150g)', protein: 34, carbs: 0, fats: 22, image: 'https://picsum.photos/seed/salmon/200' },
+];
